9912f5c94295c8fb18b578153a164bc60eb1dae7
Ziesche Til Newman <tilnewman@protonmail.com>
2023-12-07 07:47:17 -0700

    now you can specify the media path on the command line

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,35 @@
 #include "game-config.hpp"
 #include "loop-coordinator.hpp"
 
-int main(int, char *[])
-{
-    using namespace castlecrawl;
+#include <filesystem>
+#include <iostream>
+#include <string>
+#include <vector>
 
-    GameConfig config;
-    LoopCoordinator coordinator(config);
-    coordinator.playGame();
+int main(int argc, char * argv[])
+{
+    castlecrawl::GameConfig config;
+
+    const std::vector<std::string> args(argv, (argv + argc));
+    if (args.size() < 2)
+    {
+        config.media_path = std::filesystem::path{ "media" };
+    }
+    else
+    {
+        config.media_path = std::filesystem::canonical({ args.at(1) });
+    }
+
+    if (std::filesystem::exists(config.media_path))
+    {
+        castlecrawl::LoopCoordinator coordinator(config);
+        coordinator.playGame();
+    }
+    else
+    {
+        std::cout << "Unable to find the media directory at " << config.media_path.string()
+                  << std::endl;
+    }
 
     return EXIT_SUCCESS;
 }
